3ac568002b1d23658fb019ebad593f6a00b3ccc0
rkm09 <raksha.mishra09@gmail.com>
2023-09-07 12:17:00 +0530

    candy

---
+++ src/main/java/misc/Candy.java
@@ -0,0 +1,40 @@
+package misc;
+
+import javax.imageio.ImageTranscoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+/*
+Candy Store:
+input: jar = [5,8,5,8,8,8];
+Output: 3
+6 candies=> 6 / 2 = 3 pairs : (8,8), (5,5), (8,8)
+return number of boxes if all same flavored candies distributed, else -1 (initial count 2*n) arrange into n such pairs
+such that each pair has the same flavor
+ */
+public class Candy {
+    public static void main(String[] args) {
+        int[] jar = {5,8,5,8,8,8};
+//        int[] jar = {1,2,3,4};
+        int res = candy(jar);
+        System.out.println(res);
+    }
+    public static int candy(int[] jar) {
+        HashMap<Integer, Integer> hm  = new HashMap();
+        int res = 0;
+        for(int i = 0 ; i < jar.length ; i++) {
+            if(!hm.isEmpty() && hm.containsKey(jar[i])) {
+                hm.put(jar[i], hm.get(jar[i])+1);
+            } else hm.put(jar[i], 1);
+        }
+
+        for(Map.Entry<Integer,Integer> entry: hm.entrySet()) {
+            if(entry.getValue() % 2 != 0) return -1;
+            else res = res + entry.getValue();
+        }
+        return res / 2;
+    }
+}

694f322ac9ec598161ebd483561f5c4da6581c66
rkm09 <raksha.mishra09@gmail.com>
2023-09-22 17:15:50 +0530

    stack approach

--- src/main/java/leet/simple/IsSubsequence.java
+++ src/main/java/leet/simple/IsSubsequence.java
@@ -6,11 +6,11 @@ public class IsSubsequence {
     public static void main(String[] args) {
         String s = "abc";
         String t = "ahbgdc";
-        boolean b = isSubsequence(s,t);
+        boolean b = isSubsequence2(s,t);
         System.out.println(b);
     }
 //    sliding window / 2 pointer
-    public static boolean isSubsequence(String s, String t) {
+    public static boolean isSubsequence1(String s, String t) {
         int i = 0 ; int j = 0;
         while(i < s.length() && j < t.length()) {
             if(s.charAt(i) == t.charAt(j)) {
@@ -18,6 +18,20 @@ public class IsSubsequence {
             } else j++;
         }
         return i == s.length();
+    }
+//    Stack approach
+    public static boolean isSubsequence2(String s, String t) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = s.length() - 1 ; i >= 0 ; i--) {
+            stack.push(s.charAt(i));
+        }
+        for(Character ch : t.toCharArray()) {
+            if(stack.empty()) return true;
+            if(ch == stack.peek()) {
+                stack.pop();
+            }
+        }
+        return stack.empty();
     }
 }
 

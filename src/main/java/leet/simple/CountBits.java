99c1c288bd634e64f85ec81a7f11e60cf2908613
rkm09 <raksha.mishra09@gmail.com>
2023-09-23 17:33:56 +0530

    count bits

--- src/main/java/leet/simple/CountBits.java
+++ src/main/java/leet/simple/CountBits.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CountBits {
     public static void main(String[] args) {
-        int[] res = countBits(2);
+        int[] res = countBits1(2);
         Arrays.stream(res).forEach(s-> System.out.println(s));
     }
     public static int[] countBits(int n) {
@@ -23,6 +23,15 @@ public class CountBits {
         return count;
     }
 
+//    DP & last bit
+    public static int[] countBits1(int n) {
+        int[] res = new int[n+1];
+        for(int i = 1 ; i <= n ; i++) {
+            res[i] = res[i & (i-1)] + 1;
+        }
+        return res;
+    }
+
 }
 /*
 Input: n = 2
@@ -32,4 +41,8 @@ Output: [0,1,1,2,1,2]
 
 Time complexity: O(n⋅log n) For each integer x, in the worst case, we need to perform O(log n) operations, since the number of bits in x equals to log x + 1 and all the bits can be equal to 111. However, on average, each bit will be set n/2 times, so for each integer x we will perform log (x)/2 operations
 Space complexity: O(1). Since the output array does not count towards the space complexity.
+
+For DP approach:
+Time : O(n)
+Space: O(1)
  */

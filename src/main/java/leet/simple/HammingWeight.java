b619ae782251697b9d6c5d39054774099d2cb89a
rkm09 <raksha.mishra09@gmail.com>
2023-09-23 16:35:36 +0530

    hamming weight

--- src/main/java/leet/simple/HammingWeight.java
+++ src/main/java/leet/simple/HammingWeight.java
@@ -2,15 +2,27 @@ package leet.simple;
 
 public class HammingWeight {
     public static void main(String[] args) {
-        int res = hammingWeight(5);
+        int res = hammingWeight1(5);
         System.out.println(res);
     }
+//     Loop and flip :
+//    (a logical AND between any number and the mask 1 gives the least significant bit of this number)
     public static int hammingWeight(int n) {
         int mask = 1;
         int weight = 0;
         for(int i = 0 ; i < 32 ; i++) {
             if((n & mask) != 0) weight++;
             mask <<= 1;
+        }
+        return weight;
+    }
+//    Bit Manipulation:
+//    AND-ing n and n−1 flips the least-significant 1-bit to 0
+    public static int hammingWeight1(int n) {
+        int weight = 0;
+        while(n != 0) {
+            weight++;
+            n &= (n-1);
         }
         return weight;
     }

c5eb2a1c29cdc7d7c37e484677620434f7af9fa0
rkm09 <raksha.mishra09@gmail.com>
2023-09-26 22:42:06 +0530

    remove duplicates

--- src/main/java/leet/medium/RemoveDuplicateLetters.java
+++ src/main/java/leet/medium/RemoveDuplicateLetters.java
@@ -40,9 +40,9 @@ Output: "acdb"
 A string a is lexicographically smaller than a string b if in the first position where a and b differ, string a has a letter that appears earlier in the alphabet than the corresponding letter in b.
 If the first min(a.length, b.length) characters do not differ, then the shorter string is the lexicographically smaller one.
 
-Time Complexity :
+(Stack solution)
 Time complexity : O(n). Although there is a loop inside a loop, the time complexity is still O(n). This is because the inner while loop is bounded by the total number of elements added to the stack (each time it fires an element goes). This means that the total amount of time spent in the inner loop is bounded by O(n), giving us a total time complexity of O(n)
-
 Space complexity : O(1). At first glance it looks like this is O(n), but that is not true! seen will only contain unique elements, so it's bounded by the number of characters in the alphabet (a constant). You can only add to stack if an element has not been seen, so stack also only consists of unique elements. This means that both stack and seen are bounded by constant, giving us O(1) space complexity.
-Space Complexity:
+
+Identical to smallest subsequence lexicographical ordering problem
  */

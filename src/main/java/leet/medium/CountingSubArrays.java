8fed50abdc1d2bbc67a83f74a86c5d8ae4b2d775
rkm09 <raksha.mishra09@gmail.com>
2023-09-29 23:22:43 +0530

    counting sub arrays

--- src/main/java/leet/medium/CountingSubArrays.java
+++ src/main/java/leet/medium/CountingSubArrays.java
@@ -5,26 +5,40 @@ import java.util.Stack;
 
 public class CountingSubArrays {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
-        int[] nums1 = {1,3,5,4,4,6};
-        long count = countSubArrays(nums1);
+        int[] nums = {1, 2, 3, 4, 5};
+        int[] nums1 = {1, 3, 5, 4, 4, 6};
+        long count = countSubArrays1(nums1);
         System.out.println(count);
     }
 
-//    DP. time complexity O(n) and space O(1)
+    //    DP. time complexity O(n) and space O(1)
     public static long countSubArrays(int[] nums) {
         long currSubArray = 1;
         long subArrayCnt = 1;
-        for(int i = 1 ; i < nums.length ; i++) {
-            if(nums[i] > nums[i-1]) currSubArray++;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) currSubArray++;
             else currSubArray = 1;
             subArrayCnt += currSubArray;
         }
         return subArrayCnt;
     }
 
-}
+//    Greedy, O(n) despite the nested loop, each element is only iterated over once as i is strictly increasing; space O(1)
+//    sum of n natural numbers: n(n+1)/2 [increasing sub arrays]
+    public static long countSubArrays1(int[] nums) {
+        long subArrayCnt = 0;
+        for (int i = 0 ; i < nums.length ; i++) {
+            long currSubArray = 1;
+            while(i+1 < nums.length && nums[i] < nums[i+1]) {
+                currSubArray++;
+                i++;
+            }
+            subArrayCnt += ((currSubArray) * (currSubArray + 1)) / 2;
+        }
+        return subArrayCnt;
+    }
 
+}
 /*
 You are given an array nums consisting of positive integers.
 Return the number of sub arrays of nums that are in strictly increasing order.

37793afadee177dedb7b820e72fda08e56365409
rkm09 <raksha.mishra09@gmail.com>
2023-10-01 08:38:44 +0530

    reverse words

---
+++ src/main/java/leet/simple/ReverseWords.java
@@ -0,0 +1,36 @@
+package leet.simple;
+
+import java.util.Stack;
+
+public class ReverseWords {
+    public static void main(String[] args) {
+        String s = "God Ding";
+        String s1 = "Let's take LeetCode contest";
+        String res = reverseWords(s);
+        System.out.println(res);
+    }
+    public static String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        String s1 = sb.reverse().toString();
+        String[] sarr = s1.split(" ");
+        StringBuilder sb1 = new StringBuilder();
+        for(int i = sarr.length - 1 ; i >= 0 ; i--) sb1.append(sarr[i]).append(" ");
+        return sb1.substring(0, sb1.length() - 1);
+    }
+}
+/*
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+Input: s = "God Ding"
+Output: "doG gniD"
+
+Constraints:
+1 <= s.length <= 5 * 104
+s contains printable ASCII characters.
+s does not contain any leading or trailing spaces.
+There is at least one word in s.
+All the words in s are separated by a single space.
+ */

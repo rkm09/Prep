b40175c30cb375d3ecd2a212484a5f6b1d3c9840
rkm09 <raksha.mishra09@gmail.com>
2023-10-19 12:59:58 +0530

    partial singleton

--- src/main/java/designpatterns/Singleton.java
+++ src/main/java/designpatterns/Singleton.java
@@ -1,4 +1,11 @@
 package designpatterns;
 
 public class Singleton {
+    private static Singleton instance;
+    private Singleton() {}
+    public Singleton getInstance() {
+        if(instance == null)
+            return new Singleton();
+        return instance;
+    }
 }

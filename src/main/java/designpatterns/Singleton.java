422425868514a412a6746424bd8c81d244a45eda
rkm09 <raksha.mishra09@gmail.com>
2023-10-21 09:18:02 +0530

    singletone

--- src/main/java/designpatterns/Singleton.java
+++ src/main/java/designpatterns/Singleton.java
@@ -9,3 +9,17 @@ public class Singleton {
         return instance;
     }
 }
+
+// double checked lock
+class Singleton1 {
+    private static Singleton1 instance1;
+    private Singleton1(){}
+    public Singleton1 getInstance1() {
+        if(instance1 == null) {
+            synchronized (Singleton1.class) {
+                return new Singleton1();
+            }
+        }
+        return instance1;
+    }
+}

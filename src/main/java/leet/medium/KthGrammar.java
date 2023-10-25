64df6e11ee94c66d1e91f3bf58032b8e62529d05
rkm09 <raksha.mishra09@gmail.com>
2023-10-25 22:59:41 +0530

    Kth grammar

--- src/main/java/leet/medium/KthGrammar.java
+++ src/main/java/leet/medium/KthGrammar.java
@@ -7,7 +7,7 @@ import java.util.Stack;
 public class KthGrammar {
     public static void main(String[] args) {
         int n = 2, k = 2;
-        int res = kthGrammar(n, k);
+        int res = kthGrammar2(n, k);
         System.out.println(res);
     }
 
@@ -25,6 +25,43 @@ public class KthGrammar {
             int nextRootVal = (rootval == 0) ? 0 : 1;
             return depthFirstSearch(n - 1, k, nextRootVal);
         }
+    }
+
+//    Recursion:: complexity same
+    public static int kthGrammar1(int n, int k) {
+       return recursion(n, k);
+    }
+    public static int recursion(int n, int k) {
+        if(n == 1) return 0;
+        int totalCount = (int) Math.pow(2, n - 1);
+        int halfCount = totalCount / 2;
+
+        if(k > halfCount) {
+            return 1 - recursion(n-1, k - halfCount);
+        }
+        return recursion(n-1, k);
+    }
+
+//    Recursion to iteration:: time: O(n), space: O(1)
+    public static int kthGrammar2(int n, int k) {
+        if(n == 1) return 0;
+        int symbol = 1;
+        for(int currRow = n ; currRow > 1 ; --currRow) {
+            int totalCount = (int) Math.pow(2, currRow - 1);
+            int halfCount = totalCount / 2;
+
+            if (k > halfCount) {
+                symbol = 1 - symbol;
+                k -= halfCount;
+            }
+        }
+        if(symbol != 0) return 0;
+        return 1;
+    }
+//    Math; time: O(logk), space: O(1)
+    public int kthGrammar3(int n, int k) {
+        int count = Integer.bitCount(k-1);
+        return count % 2 == 0 ? 0 : 1;
     }
 }
 
@@ -59,4 +96,13 @@ Constraints:
 
 1 <= n <= 30
 1 <= k <= 2n - 1
+
+
+Recursion:
+Firstly, after generating a few rows using the steps given in the problem description, we can observe two patterns:
+
+1. The previous row is used as the prefix of the next row.
+2. If we divide any row into two equal halves then the symbol at each position will be opposite of each other in both halves (i.e. if we have a 0 in the left half at index i, then the right half will have a 1 at index i, and vice versa).
+
+
  */

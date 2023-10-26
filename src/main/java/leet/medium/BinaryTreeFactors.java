0f2322495ff38f1004959f845f1dc39d9a856495
rkm09 <raksha.mishra09@gmail.com>
2023-10-26 21:55:23 +0530

    binary tree factors

--- src/main/java/leet/medium/BinaryTreeFactors.java
+++ src/main/java/leet/medium/BinaryTreeFactors.java
@@ -65,4 +65,12 @@ Constraints:
 1 <= arr.length <= 1000
 2 <= arr[i] <= 109
 All the values of arr are unique.
+
+Let dp[i] be the number of ways to have a root node with value A[i].
+
+Since in the above example we always have x < v and y < v, we can calculate the values of dp[i] in increasing order using dynamic programming.
+
+For some root value A[i], let's try to find candidates for the children with values A[j] and A[i] / A[j] (so that evidently A[j] * (A[i] / A[j]) = A[i]). To do this quickly, we will need index which looks up this value: if A[k] = A[i] / A[j], then index[A[i] / A[j]] = k.
+
+After, we'll add all possible dp[j] * dp[k] (with j < i, k < i) to our answer dp[i]. In our Java implementation, we carefully used long so avoid overflow issues.
  */

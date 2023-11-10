acfec639022cd70f87d1ce4aab102a8b18503d8d
rkm09 <raksha.mishra09@gmail.com>
2023-11-10 08:36:42 +0530

    ransom notes

--- src/main/java/top150/easy/RansomNotes.java
+++ src/main/java/top150/easy/RansomNotes.java
@@ -6,24 +6,55 @@ import java.util.Map;
 public class RansomNotes {
     public static void main(String[] args) {
         String ransomNote = "aa"; String magazine = "ab";
-        System.out.println(canConstruct(ransomNote, magazine));
+        System.out.println(canConstruct1(ransomNote, magazine));
     }
+
+
+
+//    2 hashmaps
     public static boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> hcount = new HashMap<>();
-        for(char c : magazine.toCharArray()) {
-            hcount.put(c, hcount.getOrDefault(c, 0) + 1);
+
+        if(magazine.length() < ransomNote.length()) {
+            return false;
         }
-        Map<Character, Integer> rcount = new HashMap<>();
-        for(char c : ransomNote.toCharArray()) {
-            rcount.put(c, rcount.getOrDefault(c, 0) + 1);
+
+        Map<Character, Integer> magazineCounts = makeCountsMap(magazine);
+        Map<Character, Integer> ransomCounts = makeCountsMap(ransomNote);
+
+        for(char c : ransomCounts.keySet()) {
+            if(!magazineCounts.containsKey(c)) return false;
+            if(magazineCounts.get(c) < ransomCounts.get(c)) return false;
         }
-        for(char c : rcount.keySet()) {
-            if(!hcount.containsKey(c)) return false;
-            if(hcount.get(c) < rcount.get(c)) return false;
-        }
+
         return true;
     }
-    
+
+    private static Map<Character, Integer> makeCountsMap(String s) {
+        Map<Character, Integer> counts = new HashMap<>();
+        for(char c : s.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+        return counts;
+    }
+
+    //    1 hashmap [performs worse than 2 hashmap at runtime]
+    public static boolean canConstruct1(String ransomNote, String magazine) {
+        if(magazine.length() < ransomNote.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> magazineCounts = makeCountsMap(magazine);
+        for(char c : ransomNote.toCharArray()) {
+            int countInMagazine = magazineCounts.getOrDefault(c, 0);
+            if(countInMagazine == 0) {
+                return false;
+            }
+            magazineCounts.put(c, countInMagazine - 1);
+        }
+
+        return true;
+    }
+
 }
 /*
 Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
@@ -50,4 +81,27 @@ Constraints:
 
 1 <= ransomNote.length, magazine.length <= 105
 ransomNote and magazine consist of lowercase English letters.
+
+Approach: 2 hashmap
+The basic HashMap operations, get(...) and put(...), are O(1) time complexity.
+
+Time Complexity : O(m).
+
+When m<n, we immediately return false. Therefore, the worst case occurs when m≥nm ≥ nm≥n.
+
+Creating a HashMap of counts for the magazine is O(m), as each insertion/ count update is is O(1), and is done for each of the mmm characters.
+
+Likewise, creating the HashMap of counts for the ransom note is O(n).
+
+We then iterate over the ransom note HashMap, which contains at most nnn unique values, looking up their counterparts in the magazine `HashMap. This is, therefore, at worst O(n).
+
+This gives us O(n)+O(n)+O(m). Now, remember how we said m≥n. This means that we can simplify it to O(m)+O(m)+O(m)=3⋅O(m), dropping the constant of 3.
+
+Space Complexity : O(k) / O(1).
+
+We build two HashMaps of counts; each with up to k characters in them. This means that they take up O(k) space.
+
+For this problem, because kkk is never more than 26, which is a constant, it'd be reasonable to say that this algorithm requires O(1) space.
+
+
  */

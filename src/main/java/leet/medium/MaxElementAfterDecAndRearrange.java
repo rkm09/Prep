cbb774569eacaa463162611734ce1f9a6e6f5707
rkm09 <raksha.mishra09@gmail.com>
2023-11-21 21:47:42 +0530

    max elem after

--- src/main/java/leet/medium/MaxElementAfterDecAndRearrange.java
+++ src/main/java/leet/medium/MaxElementAfterDecAndRearrange.java
@@ -4,12 +4,26 @@ import java.util.Arrays;
 
 public class MaxElementAfterDecAndRearrange {
     public static void main(String[] args) {
-        int[] arr = {2,2,1,2,1};
+        int[] arr = {1,2,3,4};
         System.out.println(maximumElementAfterDecrementingAndRearranging(arr));
     }
 
-//   greedy; time: O(nlogn), space: O(logn) for quick sort variant in arrays.sort()
+//    counting sort; time: O(n), space: O(n)
     public static int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        int n = arr.length;
+        int[] counts = new int[n + 1];
+        for(int num : arr) {
+            counts[Math.min(num, n)]++;
+        }
+        int ans = 1;
+        for(int num = 2 ; num <= n ; num++) {
+            ans = Math.min(ans + counts[num], num);
+        }
+        return ans;
+    }
+
+//   greedy; time: O(nlogn), space: O(logn) for quick sort variant in arrays.sort()
+    public static int maximumElementAfterDecrementingAndRearranging1(int[] arr) {
         Arrays.sort(arr);
         int ans = 1;
         for(int i = 1 ; i < arr.length ; i++) {

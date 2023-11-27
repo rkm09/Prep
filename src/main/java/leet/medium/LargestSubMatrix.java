e55160ef5410cff656314d3a6537e9cb39ca526d
rkm09 <raksha.mishra09@gmail.com>
2023-11-27 07:05:03 +0530

    largest sub matrix

--- src/main/java/leet/medium/LargestSubMatrix.java
+++ src/main/java/leet/medium/LargestSubMatrix.java
@@ -5,10 +5,33 @@ import java.util.Arrays;
 public class LargestSubMatrix {
     public static void main(String[] args) {
         int[][] matrix = {{0,0,1},{1,1,1},{1,0,1}};
-        System.out.println(largestSubmatrix(matrix));
+        System.out.println(largestSubmatrix1(matrix));
     }
 
-//    sort; time: O(m * nlogn), space: O(m*n) [as we did in place modification]
+//    sort; time: O(m * nlogn), space: O(m) [as we did not modify the input]
+    public static int largestSubmatrix1(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int ans = 0;
+        int[] prevRow = new int[n];
+        for(int row = 0 ; row < m ; row++) {
+            int[] currRow = matrix[row].clone();
+            for(int col = 0 ; col < n ; col++) {
+                if(currRow[col] != 0) {
+                    currRow[col] += prevRow[col];
+                }
+            }
+            int[] sortedRow = currRow.clone();
+            Arrays.sort(sortedRow);
+            for(int i = 0 ; i < n ; i++) {
+                ans = Math.max(ans, sortedRow[i] * (n - i));
+            }
+            prevRow = currRow;
+        }
+        return ans;
+    }
+
+//    sort; time: O(m * nlogn), space: O(m*n) [as we did in place modification (not recommended)]
     public static int largestSubmatrix(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;

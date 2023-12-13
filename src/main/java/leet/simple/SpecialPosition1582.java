0834cf1591d621a1d825b9fae518101a6340b904
rkm09 <raksha.mishra09@gmail.com>
2023-12-13 14:32:38 +0530

    special pos

--- src/main/java/leet/simple/SpecialPosition1582.java
+++ src/main/java/leet/simple/SpecialPosition1582.java
@@ -3,10 +3,37 @@ package leet.simple;
 public class SpecialPosition1582 {
     public static void main(String[] args) {
         int[][] mat = {{1,0,0},{0,0,1},{1,0,0}};
-        System.out.println(numSpecial(mat));
+        System.out.println(numSpecial1(mat));
     }
 
-//    brute force; time: O(m.n(m+n)), space: O(1)
+//    precompute; time: O(m*n), space: O(m+n)
+    public static int numSpecial1(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+        int ans = 0;
+        for(int row = 0 ; row < m ; row++) {
+            for(int col = 0 ; col < n ; col++) {
+                if(mat[row][col] == 1) {
+                    rowCount[row]++;
+                    colCount[col]++;
+                }
+            }
+        }
+        for(int row = 0 ; row < m ; row++) {
+            for(int col = 0 ; col < n ; col++) {
+                if(mat[row][col] == 1) {
+                    if(rowCount[row] == 1 && colCount[col] == 1) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+//    brute force; time: O(m*n(m+n)), space: O(1)
     public static int numSpecial(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;

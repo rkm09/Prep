189834c7869eb925d7dcf29b74a44ff1fe26cc6a
rkm09 <raksha.mishra09@gmail.com>
2023-12-17 16:36:25 +0530

    food rating

---
+++ src/main/java/leet/medium/FoodRatingSystem2353.java
@@ -0,0 +1,69 @@
+package leet.medium;
+
+public class FoodRatingSystem2353 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class FoodRatings {
+    String[] foods, cuisines;
+    int[] ratings;
+
+    public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+        this.foods = foods;
+        this.cuisines = cuisines;
+        this.ratings = ratings;
+    }
+
+    public void changeRating(String food, int newRating) {
+        for(int i = 0 ; i < foods.length ; i++) {
+            if(foods[i].equals(food)) {
+                ratings[i] = newRating;
+            }
+        }
+    }
+
+    public String highestRated(String cuisine) {
+        for(int i = 0 ; i < cuisines.length ; i++) {
+            if(cuisines[i].equals(cuisine)) {
+                return foods[i];
+            }
+        } return "";
+    }
+}
+
+
+/*
+Design a food rating system that can do the following:
+
+Modify the rating of a food item listed in the system.
+Return the highest-rated food item for a type of cuisine in the system.
+Implement the FoodRatings class:
+
+FoodRatings(String[] foods, String[] cuisines, int[] ratings) Initializes the system. The food items are described by foods, cuisines and ratings, all of which have a length of n.
+foods[i] is the name of the ith food,
+cuisines[i] is the type of cuisine of the ith food, and
+ratings[i] is the initial rating of the ith food.
+void changeRating(String food, int newRating) Changes the rating of the food item with the name food.
+String highestRated(String cuisine) Returns the name of the food item that has the highest rating for the given type of cuisine. If there is a tie, return the item with the lexicographically smaller name.
+Note that a string x is lexicographically smaller than string y if x comes before y in dictionary order, that is, either x is a prefix of y, or if i is the first position such that x[i] != y[i], then x[i] comes before y[i] in alphabetic order.
+Example 1:
+
+Input
+["FoodRatings", "highestRated", "highestRated", "changeRating", "highestRated", "changeRating", "highestRated"]
+[[["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"], ["korean", "japanese", "japanese", "greek", "japanese", "korean"], [9, 12, 8, 15, 14, 7]], ["korean"], ["japanese"], ["sushi", 16], ["japanese"], ["ramen", 16], ["japanese"]]
+Output
+[null, "kimchi", "ramen", null, "sushi", null, "ramen"]
+Constraints:
+
+1 <= n <= 2 * 104
+n == foods.length == cuisines.length == ratings.length
+1 <= foods[i].length, cuisines[i].length <= 10
+foods[i], cuisines[i] consist of lowercase English letters.
+1 <= ratings[i] <= 108
+All the strings in foods are distinct.
+food will be the name of a food item in the system across all calls to changeRating.
+cuisine will be a type of cuisine of at least one food item in the system across all calls to highestRated.
+At most 2 * 104 calls in total will be made to changeRating and highestRated.
+ */

f424bfcbd1855230c0a031881d5bebad897bc0c8
rkm09 <raksha.mishra09@gmail.com>
2023-12-18 14:57:20 +0530

    min total dist

--- src/main/java/leet/hard/MinTotalDistance296.java
+++ src/main/java/leet/hard/MinTotalDistance296.java
@@ -7,12 +7,49 @@ import java.util.List;
 public class MinTotalDistance296 {
     public static void main(String[] args) {
         int[][] grid = {{1,0,0,0,1},{0,0,0,0,0},{0,0,1,0,0}};
-        int res = minTotalDistance(grid);
+        int res = minTotalDistance1(grid);
         System.out.println(res);
     }
+
 //    1 0 0 0 1
 //    0 0 0 0 0
 //    0 0 1 0 0
+
+//    median but without explicit sorting; time: O(mn), space: O(mn)
+    public static int minTotalDistance1(int[][] grid) {
+        List<Integer> rows = collectRows(grid);
+        List<Integer> cols = collectCols(grid);
+        int row = rows.get(rows.size() / 2);
+        int col = cols.get(cols.size() / 2);
+        System.out.println(rows);
+        System.out.println(cols);
+        return minDistance1D(rows, row) + minDistance1D(cols, col);
+    }
+
+    private static List<Integer> collectRows(int[][] grid) {
+        List<Integer> rows = new ArrayList<>();
+        for(int row = 0 ; row < grid.length ; row++) {
+            for(int col = 0 ; col < grid[0].length ; col++) {
+                if(grid[row][col] == 1) {
+                    rows.add(row);
+                }
+            }
+        }
+        return rows;
+    }
+
+    private static List<Integer> collectCols(int[][] grid) {
+        List<Integer> cols = new ArrayList<>();
+        for(int col = 0 ; col < grid[0].length ; col++) {
+            for(int row = 0 ; row < grid.length ; row++) {
+                if(grid[row][col] == 1) {
+                    cols.add(col);
+                }
+            }
+        }
+        return cols;
+    }
+
 //    median; time: O(mnlogmn), space: O(mn)
     public static int minTotalDistance(int[][] grid) {
         List<Integer> rows = new ArrayList<>();

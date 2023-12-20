f3d2255fc3e8460aa42255a0cab007f78564f2e3
rkm09 <raksha.mishra09@gmail.com>
2023-12-20 12:24:22 +0530

    longest subseq

---
+++ src/main/java/top150/medium/LongestIncreasingSubseq300.java
@@ -0,0 +1,50 @@
+package top150.medium;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubseq300 {
+    public static void main(String[] args) {
+        int[] nums = {10,9,2,5,3,7,101,18};
+        int[] nums1 = {7,7,7,7,7,7,7};
+        int[] nums2 = {11,12,13,14,15,6,7,8,101,18};
+        System.out.println(lengthOfLIS(nums2));
+    }
+
+//    dp; time: O(n^2), space: O(n)
+    public static int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        for(int i = 1; i < n ; i++) {
+            for(int j = 0 ; j < i ; j++) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int longest = 0;
+        for(int num : dp) {
+            longest = Math.max(longest, num);
+        }
+        return longest;
+    }
+}
+
+/*
+Given an integer array nums, return the length of the longest strictly increasing
+subsequence.
+Example 1:
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+Example 3:
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+Constraints:
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
+Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+ */

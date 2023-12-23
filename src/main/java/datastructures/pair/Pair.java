436e661ae4163ab05d51a1dbcb8508075cbf927b
rkm09 <raksha.mishra09@gmail.com>
2023-12-23 11:50:44 +0530

    pair update

--- src/main/java/datastructures/pair/Pair.java
+++ src/main/java/datastructures/pair/Pair.java
@@ -4,7 +4,7 @@ package datastructures.pair;
 import java.io.Serializable;
 
 public class Pair<K,V> implements Serializable {
-    private static final long serialVersionUID = 1L;
+//    private static final long serialVersionUID = 1L;
     public K key;
     public V value;
     public Pair(K key, V value) {
@@ -16,5 +16,26 @@ public class Pair<K,V> implements Serializable {
     }
     public V getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == this) return true;
+        if(!(o instanceof Pair)) return false;
+        Pair<K,V> other = (Pair) o;
+        boolean pairKeyEquals = ((this.key == null && other.getKey() == null) || this.key != null && this.key == other.getKey());
+        return this.value == other.getValue() && pairKeyEquals;
+    }
+
+    @Override
+    public final int hashCode() {
+        int result = 20;
+        if(key != null) {
+            result += key.hashCode();
+        }
+        if(value != null) {
+            result += value.hashCode();
+        }
+        return result;
     }
 }
